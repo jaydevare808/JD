080ac9e14b06c2e0c1688a019e5afc0b916da553
jaydevare808 <133872323+jaydevare808@users.noreply.github.com>
2026-09-25 21:45:39 +0530

    Add AlertDialog import for execution settings

--- JDCodeStudio/app/src/main/java/com/jd/codestudio/MainActivity.java
+++ JDCodeStudio/app/src/main/java/com/jd/codestudio/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jd.codestudio;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
